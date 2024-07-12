6a0730228c0fc667d66f113632f9b346c91e32df
vengateswaran28 <vengatesh2807@gmail.com>
2024-07-12 15:00:20 -0400

    Time: 56 ms (80.81%), Space: 45.6 MB (62.63%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -1,43 +1,44 @@
 class Solution {
-    void getCount(string str, string sub, int& cnt1, int& cnt2) {
-    
-        char first = sub[0], second = sub[1];
-        int i = 1;
-        while(i < str.length()) {
-            if(i > 0 && str[i-1] == first && str[i] == second) {
-                cnt1++;
-                str.erase(i-1, 2);
-                i--;
-                continue;
+    public int maximumGain(String s, int x, int y) {
+        if(y>x)
+        {
+           return helper(s,y,x,'b','a');
+        }
+        else
+        {
+          return helper(s,x,y,'a','b');
+        }
+    }
+
+   public int helper(String s,int high,int low,char first,char second)
+    {
+        StringBuilder sb=new StringBuilder();
+        int total=0;
+
+        for(char c:s.toCharArray())
+        {
+            if(sb.length()>0 && sb.charAt(sb.length()-1)==first && c==second)
+            {
+                sb.deleteCharAt(sb.length()-1);
+                total+=high;
             }
-            i++;
+            else {
+                sb.append(c);
+            }
+        }
+        StringBuilder result=new StringBuilder();
+        for(char c:sb.toString().toCharArray())
+        {
+            if(result.length()>0 && result.charAt(result.length()-1)==second && c==first)
+            {
+                result.deleteCharAt(result.length()-1);
+                total+=low;
+            }
+            else {
+                result.append(c);
+            }
         }
 
-        i = 1;
-        while(i < str.length()) {
-            if(i > 0 && str[i-1] == second && str[i] == first) {
-                cnt2++;
-                str.erase(i-1, 2);
-                i--;
-                continue;
-            }
-            i++;
-        }
-        return;
+        return total;
     }
-public:
-    int maximumGain(string s, int x, int y) {
-        
-        int mxABcnt = 0;
-        int mxBAcnt = 0;
-        int minBAcnt = 0;
-        int minABcnt= 0;
-
-        getCount(s, "ab", mxABcnt, minBAcnt);
-        getCount(s, "ba", mxBAcnt, minABcnt);
-
-        int operation1 = mxABcnt * x + minBAcnt * y;
-        int operation2 = mxBAcnt * y + minABcnt * x;
-        return max(operation1, operation2);
-    }
-};
+}
