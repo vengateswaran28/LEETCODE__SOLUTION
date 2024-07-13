9fabda2a4361aa6f1bb61b38c5d119e2e540bfbe
vengateswaran28 <vengatesh2807@gmail.com>
2024-07-12 23:23:02 -0400

    Time: 1 ms (86.65%), Space: 40.7 MB (40.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,23 @@
 class Solution {
-  public int mySqrt(long x) {
-    long l = 1;
-    long r = x + 1;
+    public int mySqrt(int x) {
+     int low = 1, high = x;
+        if((x==0)||(x==1)){
+            return x;
+        }
 
-    while (l < r) {
-      final long m = (l + r) / 2;
-      if (m > x / m)
-        r = m;
-      else
-        l = m + 1;
+        while (low<=high) {
+            int mid = (low+high)/2;  
+            if(x/mid==mid){ //if mid*mid=x(3*3=9), then mid will be the sqrt(x)
+                return mid;
+            }
+            else if(x/mid<mid){ //if (mid*mid)<x, then the sqrt(x) is lower than mid
+                high=mid-1;
+            }
+            else if(x/mid>mid){ //if (mid*mid)<x, then the sqrt(x) is higher than mid
+                low=mid+1;
+            }
+            
+        }
+        return high; //if none of those work than high will be the ans
     }
-
-    // l: smallest number s.t. l * l > x
-    return (int) l - 1;
-  }
 }
-
-
-    
