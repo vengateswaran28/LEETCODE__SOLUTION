4009cd350326bc2759c5a93c7ba50749a812f9fe
vengateswaran28 <vengatesh2807@gmail.com>
2024-07-18 13:06:45 -0400

    Time: 1 ms (41.21%), Space: 40.7 MB (52.08%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<0){
+            return false;
+        }
+       if(n==1){
+        return true;
+       } 
+       if(n==0){
+        return false;
+       }
+       while(n>1){
+        if(n%2!=0){
+         
+            return false;
+        }
+        else{
+            n/=2;
+        }
+       }
+       return true;
+    }
+}
