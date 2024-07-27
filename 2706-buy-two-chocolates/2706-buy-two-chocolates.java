f3970a898ef4f785b5bda83bb3a4d0322c223552
vengateswaran28 <vengatesh2807@gmail.com>
2024-07-27 08:04:54 -0400

    Time: 2 ms (74.11%), Space: 44.5 MB (15.01%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -2,7 +2,7 @@ class Solution {
     public int buyChoco(int[] p, int m) {
         Arrays.sort(p);
        if(p[0]+p[1]<=m){
-        return (p[0]+p[1])-m;
+        return m-(p[0]+p[1]);
        } 
        else{
        return m;
