d1242f2c74c9d6f4152900b61abf638b090d340b
vengateswaran28 <vengatesh2807@gmail.com>
2024-07-28 14:28:39 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maximum69Number (int num) {
+          StringBuilder s= new StringBuilder(+""num);
+          for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='6'){
+                s.setCharAt(i,'9');
+                break;
+            }
+          }
+          return Integer.parseInt(s.toString());
+    }
+}
