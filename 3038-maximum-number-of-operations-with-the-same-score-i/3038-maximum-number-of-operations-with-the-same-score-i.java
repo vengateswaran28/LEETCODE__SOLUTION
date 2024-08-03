85149da2f39479a2197876c748c20f90ecd55799
vengateswaran28 <vengatesh2807@gmail.com>
2024-08-03 13:59:09 -0400

    Time: 0 ms (100%), Space: 41.9 MB (28.86%) - LeetHub

--- 3038-maximum-number-of-operations-with-the-same-score-i/3038-maximum-number-of-operations-with-the-same-score-i.java
+++ 3038-maximum-number-of-operations-with-the-same-score-i/3038-maximum-number-of-operations-with-the-same-score-i.java
@@ -3,9 +3,11 @@ class Solution {
      int sum=0;
      int c=0;
     int i=0;
+    int  sum1=nums[0]+nums[1];
            while(i<nums.length-1){
+        
             sum=nums[i]+nums[i+1];
-            if(sum==5){
+            if(sum==sum1){
                 c++;
                 i+=2;
             
